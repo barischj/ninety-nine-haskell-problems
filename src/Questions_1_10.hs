2dc7fe6b6072e58f1a57a9161fac87d008b5f572
Jeremy Barisch Rooney <barischrooney@gmail.com>
2017-08-05 22:51:32 +0100

    Cleanup Qs 1-6

--- src/Questions_1_10.hs
+++ src/Questions_1_10.hs
@@ -1,44 +1,28 @@
 module Questions_1_10 where
 
-import Data.Foldable (foldl')
+import           Safe (headMay)
 
 -- | Question 1.
-myLast :: [a] -> Maybe a
-myLast [x] = Just x
-myLast (_:xs) = myLast xs
-myLast [] = Nothing
+myLast :: Foldable f => f a -> Maybe a
+myLast = foldl (\_ x -> Just x) Nothing
 
 -- | Question 2.
-myButLast :: [a] -> Maybe a
-myButLast [x, _] = Just x
-myButLast (_:xs) = myButLast xs
-myButLast [] = Nothing
+myButLast :: Foldable f => f a -> Maybe a 
+myButLast = fst . foldl (\(x, y) z -> (y, Just z)) (Nothing, Nothing)
 
 -- | Question 3.
-elementAt :: (Eq a, Num a) => a -> [a] -> Maybe a
-elementAt n (x:xs)
-  | n == 0    = Just x
-  | otherwise = elementAt (n - 1) xs
-elementAt _ [] = Nothing
-
--- | Nth last element of a list. 0th last being last.
-nthLast :: (Eq a, Num a) => a -> [a] -> Maybe a
-nthLast n = elementAt n . reverse
+elementAt :: [a] -> Int -> Maybe a
+elementAt xs n
+  | n < 1     = Nothing
+  | otherwise = headMay $ drop (n - 1) xs
 
 -- | Question 4.
-myLength :: Num b => [a] -> b
-myLength = foldl' (\l _ -> l + 1) 0
+myLength :: Num b => Foldable f => f a -> b
+myLength = foldl (\l _ -> l + 1) 0
 
 -- | Question 5.
 myReverse :: [a] -> [a]
 myReverse = foldl (flip (:)) []
-
-foldl_ :: (a -> b -> a) -> a -> [b] -> a
-foldl_ _ n [] = n
-foldl_ f n (x:xs) = foldl_ f (f n x) xs
-
-flip_ :: (a -> b -> c) -> (b -> a -> c)
-flip_ f b a = f a b
 
 -- | Question 6.
 isPalindrome :: Eq a => [a] -> Bool
@@ -49,14 +33,14 @@ isPalindrome xs = xs == reverse xs
 data Tree a = Leaf a | Node [Tree a]
 
 flatten :: Tree a -> [a]
-flatten (Leaf x) = [x]
+flatten (Leaf x)  = [x]
 flatten (Node xs) = concatMap flatten xs
 
 concatMap_ :: (a -> [b]) -> [a] -> [b]
 concatMap_ f = concat . map f
 
 concat_ :: [[a]] -> [a]
-concat_ [] = []
+concat_ []     = []
 concat_ (x:xs) = x ++ concat_ xs
 
 compress :: Eq a => [a] -> [a]
@@ -74,5 +58,6 @@ compress' (x:xs) (Just c)
 -- compress'' = foldr cons
 --   where cons x Nothing  = [x]
 --         cons x (Just c)
---           | x == c = 
+--           | x == c =
+
 

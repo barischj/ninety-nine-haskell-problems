f411c82607f373b88ebb5af12f80afeb8a7e9d50
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2018-02-14 18:56:43 +0100

    Questions 16 - 20

--- src/Questions_11_20.hs
+++ src/Questions_11_20.hs
@@ -35,3 +35,34 @@ repli :: [a] -> Int -> [a]
 repli xs n = foldl (\acc e -> acc ++ repliX e n) [] xs
   where repliX _ 0 = []
         repliX x n = x:repliX x (n - 1)
+
+-- | Question 16.
+dropEvery :: [a] -> Int -> [a]
+dropEvery xs n =
+  map snd $ filter ((/= n) . fst) (zip (cycle [1..n]) xs)
+
+-- | Question 17.
+split :: [a] -> Int -> ([a], [a])
+split []     _ = ([],   [])
+split xs     0 = ([],   xs)
+split (x:xs) n = (x:x1, x2)
+  where (x1, x2) = split xs (n - 1)
+
+-- Question 18.
+slice :: Int -> Int -> [a] -> [a]
+slice start stop = take (stop - start + 1) . drop (start - 1)
+
+-- Question 19.
+rotate :: Int -> [a] -> [a]
+rotate n xs
+  | n > 0     = drop n xs ++ take n xs
+  | otherwise = drop (length xs + n) xs ++ take (length xs + n) xs
+
+-- Lovely alternative.
+rotate' :: Int -> [a] -> [a]
+rotate' n xs = take (length xs) $ drop (length xs + n) $ cycle xs
+
+-- Question 20.
+removeAt :: [a] -> Int -> [a]
+removeAt xs n = start ++ drop 1 end
+  where (start, end) = splitAt n xs
